d4cb7bf1d9cf72d0914c9c1f5bef291ecce6e9cd
JOHAN FRANZ REYNA AVALOS <149949315+FRANCISDAX@users.noreply.github.com>
2024-08-25 22:47:59 -0500

    upgrade

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// document.querySelectorAll('.play-sound').forEach(function(element) {
-//     element.addEventListener('click', function(event) {
-//         event.preventDefault();
-//         var audio = document.querySelector('.audio');
-//         audio.play();
-//         setTimeout(() => {
-//             window.location.href = element.href;
-//         }, 250);
-//     });
-// });
-
 document.querySelectorAll('.play-sound').forEach(function(element) {
     element.addEventListener('click', function(event) {
         event.preventDefault();
